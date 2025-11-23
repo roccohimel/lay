50d588eae8ba0849798fe1d8263710b8cd3ea622
roccohimel <rockyrbn1847@gmail.com>
2025-11-23 00:35:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cd ~/tmux
 sh autogen.sh
 ./configure && make
 sudo install -m 755 ~/tmux/tmux /usr/local/bin/tmux
-echo "Installing lay..."
+echo "Installing Lay..."
 sudo cp ~/lay/lay /usr/local/bin -v
 sudo cp ~/lay/tks /usr/local/bin -v
 sudo cp ~/lay/lay.1 /usr/share/man/man1 -v
