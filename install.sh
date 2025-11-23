57c1049d036d1b8f7cafbc126cf8f8ba134373e6
roccohimel <rockyrbn1847@gmail.com>
2025-11-23 00:09:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,17 +8,6 @@ cd ~/tmux
 sh autogen.sh
 ./configure && make
 sudo install -m 755 ~/tmux/tmux /usr/local/bin/tmux
-echo "Installing fastfetch..."
-git clone https://github.com/fastfetch-cli/fastfetch
-cd ~/fastfetch
-mkdir -p build
-cd build
-cmake ..
-cmake --build . --target fastfetch
-echo "Installing htop..."
-git clone https://github.com/htop-dev/htop
-cd htop
-./autogen.sh && ./configure && make
 echo "Installing lay..."
 sudo cp ~/lay/lay /usr/local/bin -v
 sudo cp ~/lay/tks /usr/local/bin -v
